a2ae423a482796dc94df3f944805219b7968c2c4
Elvijs <s22leiselvi@venta.lv>
2025-02-21 10:16:56 +0200

    Add: ArrayList for Classes in MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -2,49 +2,51 @@ package service;
 
 import model.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainService {
+
+	private static ArrayList<Student> allStudents = new ArrayList<>();
+	private static ArrayList<Professor> allProfessors = new ArrayList<>();
+	private static ArrayList<Course> allCourses = new ArrayList<>();
+	private static ArrayList<Grade> allGrades = new ArrayList<>();
+
+
 	public static void main(String[] args) {
 		Student st1 = new Student();
-		System.out.println(st1);
-		
 		Student st2 = new Student("Elvijs", "Leistmanis");
-		System.out.println(st2);
-		
 		Student st3 = new Student("E!$lvijs", "");
-		System.out.println(st3);
+
+		allStudents.addAll(Arrays.asList(st1, st2, st3));
+		System.out.println(allStudents);
 		
 		System.out.println("-----------------------");
 		
 		Professor p1 = new Professor();
-		System.out.println(p1);
-		
 		Professor p2 = new Professor("Karina", "Skirmante", Degree.mg);
-		System.out.println(p2);
-		
 		Professor p3 = new Professor("K95rina", "___", Degree.bsc);
-		System.out.println(p3);
+
+		allProfessors.addAll(Arrays.asList(p1, p2, p3));
+		System.out.println(allProfessors);
 
 		System.out.println("-----------------------");
 
 		Course c1 = new Course("test", 4, p2);
-		System.out.println(c1);
-
 		Course c2 = new Course();
-		System.out.println(c2);
-
 		Course c3 = new Course("", 0, p3);
-		System.out.println(c3);
+
+		allCourses.addAll(Arrays.asList(c1, c2, c3));
+		System.out.println(allCourses);
 
 		System.out.println("-----------------------");
 
 		Grade g1 = new Grade();
-		System.out.println(g1);
-
 		Grade g2 = new Grade(6, st2, c1);
-		System.out.println(g2);
-
 		Grade g3 = new Grade(0, st1, c1);
-		System.out.println(g3);
+
+		allGrades.addAll(Arrays.asList(g1, g2, g3));
+		System.out.println(allGrades);
 	}
 
 }
